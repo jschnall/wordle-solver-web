27dc4b102d752427708ca39bafe8c5478d612975
jschnall <jschnall@gmail.com>
2024-02-07 12:47:02 -0800

    Update lib to fix bug caused by mixed casing of same letter in a guess

--- src/main/kotlin/dev/wary/wordle/ui/Routing.kt
+++ src/main/kotlin/dev/wary/wordle/ui/Routing.kt
@@ -6,12 +6,15 @@ import io.ktor.server.routing.*
 
 fun Application.configureUiRouting() {
     routing {
-            staticResources("/wordlesolver", "static")
+            staticResources("/wordlesolver", "static") {
+                preCompressed(CompressedFileType.BROTLI, CompressedFileType.GZIP)
+            }
+// TODO Add option to use 6 letter word dictionary
 //            get("/wordlesolver") {
 //                call.respond(MustacheContent(
 //                    "index.mustache",
 //                    mapOf(
-//                        "hello" to "world"
+//                        "word_length" to 5
 //                    )
 //                ))
 //            }
